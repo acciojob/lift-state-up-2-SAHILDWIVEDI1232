9665886466b421c71c301cbbd865678e7fca8f72
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2026-05-19 11:19:46 +0530

    Add Children component for input handling

---
+++ src/components/Chidren.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Children({ inputValue, setInputValue }) {
+  return (
+   <div
+  className="child"
+  style={{
+    backgroundColor: "brown",
+    padding: "20px",
+  }}
+>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+    </div>
+  );
+}
+
+export default Children;
